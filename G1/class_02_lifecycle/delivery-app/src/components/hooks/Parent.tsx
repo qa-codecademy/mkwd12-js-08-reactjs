745b7e7b5a880ff964dbd89cf7808296024fb187
Ivo Kostovski <ivo.kostovski@gmail.com>
2024-06-05 20:47:47 +0200

    class 2 done + homework

--- G1/class_02_lifecycle/delivery-app/src/components/hooks/Parent.tsx
+++ G1/class_02_lifecycle/delivery-app/src/components/hooks/Parent.tsx
@@ -1,10 +1,42 @@
+import { useEffect, useState } from 'react';
+import Child from './Child';
+
 export default function Parent() {
+	const [showChild, setShowChild] = useState(true);
+	const [parentData, setParentData] = useState('Parent Data');
+
+	// only one execution on mounting
+	useEffect(() => {
+		console.log('Parent componentDidMount');
+
+		return () => {
+			console.log('Parent componentWillUnmount');
+		};
+	}, []);
+
+	// triggers each change VERY DANGEROUS
+	useEffect(() => {
+		console.log('Parent componentDidUpdate');
+	});
+
+	// triggers when parentData is changed
+	useEffect(() => {
+		console.log('Parent data has been changed');
+	}, [parentData]);
+
+	console.log('Parent render');
 	return (
 		<div>
 			<h1>Parent Component</h1>
-			<button>Toggle Child</button>
-			<button>Change Parent Data</button>
-			<button>No Render Change</button>
+			<button onClick={() => setShowChild(!showChild)}>Toggle Child</button>
+			<button
+				onClick={() => setParentData('Updated Parent Data' + Math.random())}>
+				Change Parent Data
+			</button>
+			<button onClick={() => console.log('No re-render of Parent Component')}>
+				No Render Change
+			</button>
+			{showChild && <Child parentData={parentData} />}
 		</div>
 	);
 }
