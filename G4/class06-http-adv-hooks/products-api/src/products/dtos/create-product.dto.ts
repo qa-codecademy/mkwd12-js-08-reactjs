872d4fe697b5a4497191e62993a4ee8a1b31202d
borisovski-borche <borisovski.borce@gmail.com>
2024-06-18 21:58:51 +0200

    added api

---
+++ G4/class06-http-adv-hooks/products-api/src/products/dtos/create-product.dto.ts
@@ -0,0 +1,25 @@
+import { IsNumber, IsString, IsUrl, Length, Max, Min } from 'class-validator';
+
+export class CreateProductDto {
+  @IsString()
+  @Length(3, 100)
+  title: string;
+
+  @IsNumber()
+  @Min(0)
+  stock: number;
+
+  @IsNumber()
+  @Min(0)
+  @Max(10000)
+  price: number;
+
+  @IsString()
+  description: string;
+
+  @IsUrl()
+  image: string;
+
+  @IsString()
+  category: string;
+}
