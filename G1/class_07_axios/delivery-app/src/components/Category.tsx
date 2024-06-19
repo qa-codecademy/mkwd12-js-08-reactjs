737ae1e5cfe5e472a467b63f66a95c80c56a0bd0
Ivo Kostovski <ivo.kostovski@gmail.com>
2024-06-19 19:10:55 +0200

    axios completed

--- G1/class_07_axios/delivery-app/src/components/Category.tsx
+++ G1/class_07_axios/delivery-app/src/components/Category.tsx
@@ -1,25 +1,49 @@
-import { useParams } from "react-router-dom";
-import { Dish } from "../common/types/dish.interface";
-import DishCard from "./DishCard";
-import { useContext } from "react";
-import { DishContext } from "../context/dish.context";
+import { useNavigate, useParams } from 'react-router-dom';
+import { Dish } from '../common/types/dish.interface';
+import DishCard from './DishCard';
+import { useEffect, useState } from 'react';
+import axios from 'axios';
+import Loader from './Loader';
+import { Category } from '../common/types/category.enum';
 
 export default function CategoryPage() {
-  const { dishes } = useContext(DishContext);
-  const { categoryName: category } = useParams();
+	const [dishes, setDishes] = useState<Dish[]>([]);
+	const [isLoading, setIsLoading] = useState(false);
+	const { categoryName: category } = useParams();
+	const navigate = useNavigate();
 
-  const filteredDishes: Dish[] = dishes.filter(
-    (dish) => dish.category === category
-  );
+	const isValidCategory = Object.values(Category).includes(
+		category as Category
+	);
 
-  return (
-    <div className="p-4">
-      <h2 className="text-2xl font-bold mb-4">{category}</h2>
-      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-4">
-        {filteredDishes.map((dish) => (
-          <DishCard key={dish.id} dish={dish} />
-        ))}
-      </div>
-    </div>
-  );
+	useEffect(() => {
+		if (!isValidCategory) {
+			navigate('/not-found');
+			return;
+		}
+
+		setIsLoading(true);
+		axios
+			.get(`http://localhost:3000/api/dishes?category=${category}`)
+			.then(response => setDishes(response.data.payload))
+			.catch(error =>
+				console.error('Error while fetching dishes by category', { error })
+			)
+			.finally(() => setIsLoading(false));
+	}, [category, isValidCategory, navigate]);
+
+	if (isLoading) {
+		return <Loader />;
+	}
+
+	return (
+		<div className='p-4'>
+			<h2 className='text-2xl font-bold mb-4'>{category}</h2>
+			<div className='grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-4'>
+				{dishes.map(dish => (
+					<DishCard key={dish.id} dish={dish} />
+				))}
+			</div>
+		</div>
+	);
 }
